a9c14084d4bc7eed7cce462c2762f22f6f2088a8
nicholas harding <hardingnick00@gmail.com>
2021-02-25 10:23:40 +1300

    Add project files.

---
+++ ArrayTasks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArrayTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
